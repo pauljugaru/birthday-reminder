ced2a8e2559c05eb6588cc21682985358607d880
Noje Vlad <vlad.noje@student.unitbv.ro>
2025-06-22 00:12:39 +0300

    Rezolvare limba si schimbare data corecta

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeRo from '@angular/common/locales/ro';
+
+import { NZ_I18N, ro_RO } from 'ng-zorro-antd/i18n';
+
+registerLocaleData(localeRo);
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    { provide: LOCALE_ID, useValue: 'ro' },
+    { provide: NZ_I18N, useValue: ro_RO }
+  ]
+}).catch((err) => console.error(err));
